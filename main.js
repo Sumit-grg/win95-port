bc4982f812cfcb59aeeb65abdf4b9828de5a769f
Sumit-grg <65023009+Sumit-grg@users.noreply.github.com>
2023-05-07 00:35:44 +0100

    project file done

--- main.js
+++ main.js
@@ -10,3 +10,37 @@ setInterval(() => {
   timeNow.innerHTML = `${hour} : ${minute}`;
 }, 1000);
 
+//project folder - client/personal switch tab////
+
+const clientTab = document.getElementById('client-tab');
+const personalTab = document.getElementById('personal-tab');
+         ///display
+const personalProj = document.getElementById("personal-pro");
+const clientProj = document.getElementById("client-pro");
+       ///modal-main
+const modalMain = document.getElementById("modal-main");
+
+
+clientTab.addEventListener("click", ()=> {
+  personalProj.style.display = "none";
+  clientProj.style.display = "block";
+  clientTab.style.cssText = `  border-top: 2px solid black;
+  border-right: 2px solid rgb(226, 223, 223);
+  border-bottom: 2px solid rgb(226, 223, 223);;
+  border-left: 2px solid black;background-color:rgb(228, 224, 224);`
+  personalTab.style.cssText = `background-color: rgb(184, 181, 181);
+  border: 2px solid rgb(79, 79, 79);`;
+  modalMain.style.cssText = `border-left:2px solid black; border-bottom:2px solid black; border-right: 2px solid black`;
+})
+
+personalTab.addEventListener("click", ()=>{
+  personalProj.style.display = "block";
+  clientProj.style.display = "none";
+  personalTab.style.cssText = `  border-top: 2px solid black;
+  border-right: 2px solid rgb(226, 223, 223);
+  border-bottom: 2px solid rgb(226, 223, 223);
+  border-left: 2px solid black;rgb(228, 224, 224);`;
+  clientTab.style.cssText = `background-color: rgb(184, 181, 181);
+  border: 2px solid rgb(79, 79, 79);`;
+  modalMain.style.cssText = `border-left:2px solid black; border-bottom:2px solid black; border-right: 2px solid black`;
+})
